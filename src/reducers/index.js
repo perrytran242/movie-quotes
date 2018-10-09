6df4ac0d2e497f0d34c38296ea9e6c54efab29be
Perry Tran <ptran209@gmail.com>
2018-10-09 12:47:28 -0700

    Initial sign up completed

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,8 @@
-import { combineReducers } from 'redux';
+import { combineReducers, applyMiddleware } from 'redux';
 import { reducer as formReducer } from 'redux-form';
 import userReducer from './user_reducer';
 import movieReducer from './movie_reducer';
+
 
 
 const rootReducer = combineReducers({
